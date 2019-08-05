9ed7b55932c9761c134e26ca5e9e8ac813687c7e
Richard Lee <richlee81192@gmail.com>
2019-08-05 18:20:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,7 +59,7 @@ def reduce_to_all_true(source_array)
 end
 
  def reduce_to_any_true(source_array)
-  require 'pry'
+  binding.pry
    i = 0 
    while i < source_array.length do
      return true if source_array[i] == true
